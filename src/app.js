845601240e9ee0a9afebcbc31bdc11e2d7b969a1
mohdathar42 <mohdatharkhan42@gmail.com>
2025-07-10 23:27:12 +0530

    again port no change

--- src/app.js
+++ src/app.js
@@ -29,8 +29,8 @@ app.use("/", userRouter); // Mounting user routes at /api/user
 connectDB()
   .then(() => {
     console.log("MongoDB connected successfully");
-    app.listen(30005, () => {
-      console.log("Server started on port 30005");
+    app.listen(3001, () => {
+      console.log("Server started on port 3001");
     });
   })
   .catch((err) => {
